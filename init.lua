f783a9c9ea5a2137050a56113133f19573746492
mick1221 <noah.mickus@gmail.com>
2026-04-07 21:07:09 -0500

    inital commit

---
+++ init.lua
@@ -0,0 +1,10 @@
+require("config.lazy")
+
+--set color scheme
+vim.cmd.colorscheme("catppuccin-mocha")
+
+local opt = vim.opt
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.softtabstop = 4
